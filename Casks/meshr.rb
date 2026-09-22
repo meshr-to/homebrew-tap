b61a212c6ff6a43346e40f4d54539da62059c67f
Savas AYIK <savas.ayik@powerkonnekt.com>
2026-09-22 21:10:51 +0300

    meshr v0.7.173

--- Casks/meshr.rb
+++ Casks/meshr.rb
@@ -1,6 +1,6 @@
 cask "meshr" do
   version "0.7.173"
-  sha256 "cb07d0f74541bf9eb51f830448a6e5c36c4061a05a5dd36f1cbc37810378333e"
+  sha256 "84da2b72d2c0bd4f28ecb951b711e72bd394e42054773ae628eb74a4fbe133d1"
 
   url "https://get.meshr.to/releases/v#{version}/Meshr-v#{version}-macOS.dmg"
   name "Meshr"
